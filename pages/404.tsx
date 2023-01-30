6da7e979b84ab759f2415f4b909f223c1af1f38c
Nikita <nikitkavershinin2019@gmail.com>
2023-01-30 12:32:38 +0300

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,12 @@ import { NextPage } from 'next'
 import Error from '../app/components/screens/Error.tsx/Error'
 
 const ErrorPage: NextPage = () => {
-	return <Error />
+	return (
+		<>
+			<Error />
+			<p>Error</p>
+		</>
+	)
 }
 
 export default ErrorPage
